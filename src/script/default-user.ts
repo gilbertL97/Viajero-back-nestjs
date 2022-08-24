08b8087ce9d53b190469932d372e66025e89ffa2
Gilbert Luis <gilbertluis97@gmail.com>
2022-08-24 15:18:48 -0400

    ultimos cambios

---
+++ src/script/default-user.ts
@@ -0,0 +1,30 @@
+import { getRepository } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { Configuration } from 'src/config/config.const';
+import { UserEntity } from 'src/user/entity/user.entity';
+
+const setDefaultUser = async (config: ConfigService) => {
+  const userRepository = getRepository<UserEntity>(UserEntity);
+
+  const defaultUser = await userRepository
+    .createQueryBuilder()
+    .where('name = :email', {
+      email: config.get<string>(Configuration.DEFAULT_ADMIN_USER),
+    })
+    .getOne();
+
+  if (!defaultUser) {
+   
+    const adminUser = userRepository.create({
+      name: config.get<string>(Configuration.DEFAULT_ADMIN_USER),
+      email: config.get<string>(Configuration.DEFAULT_ADMIN_email),
+      password: config.get<string>(Configuration.DEFAULT_ADMIN_PASSW),
+      role: 'admin',
+      active: true,
+    });
+
+    return await userRepository.save(adminUser);
+  }
+};
+
+export default setDefaultUser;
