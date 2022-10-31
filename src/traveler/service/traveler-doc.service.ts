c39b73daebeb28abc811f5f13cf7fc62083758d9
Gilbert Luis <gilbertluis97@gmail.com>
2022-10-31 17:08:02 -0400

    terminando la generacion del pdf

--- src/traveler/service/traveler-doc.service.ts
+++ src/traveler/service/traveler-doc.service.ts
@@ -22,74 +22,131 @@ export class TravelerDocService {
   ) {}
 
   async createTestPDf(traveler: TravelerEntity): Promise<Buffer> {
+    
+    /*const text='EXCLUSIONES
+    Las prestaciones que no hayan sido solicitadas al Asegurador a través de la
+    Central de Alarma de ASISTUR y efectuadas sin su acuerdo, salvo en caso
+    de fuerza mayor o de imposibilidad material demostrada.
+    La realización de la prueba diagnóstico para COVID-19 (PCR) o controles
+    preventivos sanitarios al Viajero, establecidos en el protocolo médico por las
+    autoridades sanitarias.
+    Participación en competencias peligrosas de cualquier índole, así como los
+    entrenamientos o pruebas, las apuestas y las consecuencias que
+    sobrevengan de la práctica de pasatiempos peligrosos o de alto riesgo,
+    incluyendo, pero no limitado a: caza, actividades subacuáticas, aladeltismo,
+    parasail, alpinismo, motociclismo, automovilismo, boxeo, vehículos todo
+    terreno, etc. Esta exclusión podrá quedar sin efecto mediante el pago de una
+    extraprima.
+    Enfermedades crónicas y/o preexistentes excepto cuando se trate de
+    atención médica por urgencia médica. Quedan expresamente excluidos los
+    estudios y/o los tratamientos relacionados con enfermedades crónicas o
+    preexistentes o congénitas o recurrentes, conocidas o no por el Viajero
+    padecidas con anterioridad al inicio de la vigencia de este seguro y/o del viaje,
+    lo que sea posterior, así como sus agudizaciones, secuelas o consecuencias
+    (incluso cuando las mismas aparezcan durante el viaje).
+    Las consecuencias de las intervenciones quirúrgicas no motivadas por un
+    accidente.
+    Estas COBERTURAS y EXCLUSIONES son a título informativo. Usted debe
+    leer las CONDICIONES GENERALES para informarse detalladamente de las
+    características del Seguro de Viaje.
+    Cancelación del Seguro: Después de transcurridas 72h antes de la fecha de
+    inicio del seguro, el Viajero NO podrá hacer efectiva la cancelación.'*/
+    const pantoneColor='#1b1462';
+    const  blackColor='black';
+    const font='Helvetica';
+    const fontBold='Helvetica-Bold';
+    const timeFormat: Intl.DateTimeFormatOptions = {  year: 'numeric', month: 'long', day: 'numeric' }
+    const templatePrice = traveler.coverage.daily ? traveler.coverage.price+' $USD pax/dia':traveler.coverage.price+' $USD x dias'
+
     const pdfBuffer: Buffer = await new Promise((resolve) => {
       const doc = new PDFDocument({
         size: 'LETTER',
         bufferPages: true,
       });
       doc
-        .fillColor('blue')
-        .font('Helvetica-Bold')
-        .lineGap(1)
+        .fillColor(pantoneColor)
+        .font(fontBold)
         .fontSize(15)
         .text('SEGURO DE ASISTENCIA AL VIAJERO (VIAJES IN)');
       doc.text('CERTIFICADO DE SEGURO');
-      doc.image('assets/esicubaLogo.jpg', 440, 60, { scale: 0.25 });
+      doc.image('assets/esicubaLogo.jpg', 460, 75, { scale: 0.20 });
       doc
         .fillColor('gray')
         .fontSize(12)
-        .font('Helvetica')
+        .font(font)
         .text('Válido en el territorio nacional de la República de Cuba', {});
+        doc.moveDown();
+        doc.moveDown();
+
       doc
-        .fillColor('blue')
+        .fillColor(pantoneColor)
         .fontSize(12)
-        .font('Helvetica-Bold')
-        .text('ASEGURADOR :', {
+        .font(fontBold)
+        .text('ASEGURADOR: ', {
           continued: true,
         });
       doc
-        .fillColor('black')
+        .fillColor(blackColor)
 
-        .font('Helvetica')
-        .text(' Seguros Internacionales de Cuba, S.A. (ESICUBA)');
+        .font(font)
+        .text('Seguros Internacionales de Cuba, S.A. (ESICUBA)');
       doc
-        .fillColor('blue')
+        .fillColor(pantoneColor)
 
-        .font('Helvetica-Bold')
-        .text('No Poliza : ', {
+        .font(fontBold)
+        .text('No Poliza: ', {
           continued: true,
         });
       doc
-        .fillColor('black')
-        .font('Helvetica')
-        .text(traveler.contractor.poliza + '  ', {
-          continued: true,
-        });
-      doc.fillColor('blue').font('Helvetica-Bold').text('Cliente : ', {
+        .fillColor(blackColor)
+        .font(font)
+        .text(traveler.contractor.poliza);
+      doc.fillColor(pantoneColor).font(fontBold).text('Cliente: ', {
         continued: true,
       });
-      doc.fillColor('black').font('Helvetica').text(traveler.contractor.client);
-      doc.fillColor('blue').font('Helvetica-Bold').text('Contratante: ', {
+      doc.fillColor(blackColor).font(font).text(traveler.contractor.client);
+      doc.fillColor(pantoneColor).font(fontBold).text('Contratante: ', {
         continued: true,
       });
-      doc.fillColor('black').font('Helvetica').text(traveler.name, {
+      doc.fillColor(blackColor).font(font).text(traveler.name);
+      doc.fillColor(pantoneColor).font(fontBold).text('Pasaporte: ', {
         continued: true,
       });
-      doc.fillColor('blue').font('Helvetica-Bold').text(' Pasaporte: ', {
-        continued: true,
-      });
-      doc.fillColor('black').font('Helvetica').text(traveler.passport);
+      doc.fillColor(blackColor).font(font).text(traveler.passport);
       doc
-        .fillColor('blue')
-        .font('Helvetica-Bold')
-        .text('Vigencia del Seguro :   ', {
-          continued: true,
-        });
+        .fillColor(pantoneColor)
+        .font(fontBold)
+        .text('Vigencia del Seguro ');
       doc.text('Desde : ', {
         continued: true,
       });
-      doc.fillColor('black').font('Helvetica');
-      //  .text(traveler.start_date.toDateString.toString());
+      doc.fillColor(blackColor).font(font)
+      .text(new Date(traveler.start_date).toLocaleDateString('ES',timeFormat), {
+        continued: true,
+      });
+      doc
+      .fillColor(pantoneColor)
+      .font(fontBold)
+      .text('    Hasta: ', {
+        continued: true,
+      });
+      doc.fillColor(blackColor).font(font)
+      .text(new Date(traveler.end_date_policy).toLocaleDateString('ES',timeFormat));
+      doc
+      .fillColor(pantoneColor)
+      .font(fontBold)
+      .text('Precio del seguro: ', {
+        continued: true,
+      });
+      doc.fillColor(blackColor).font(font).text(templatePrice);
+      doc
+      .fillColor(pantoneColor)
+      .font(fontBold)
+      .text('Plan de Viaje: ', {
+        continued: true,
+      });
+      doc.fillColor(blackColor).font(font).text(traveler.coverage.name);
+      //doc.text()
       const buffer = [];
       doc.on('data', buffer.push.bind(buffer));
       doc.on('end', () => {
