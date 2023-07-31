442ab5cb7e9f61e92aeb949c4eee5c11ccd4ae7e
Gilbert Luis <gilbertluis97@gmail.com>
2023-07-31 16:22:51 -0400

    agregando seguridad a swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,27 @@
 import { Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { GetUser } from 'src/common/decorator/user.decorator';
+import { LoginUserDto } from 'src/user/dto';
 
 import { UserEntity } from 'src/user/entity/user.entity';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guard/local.auth.guard';
 
 @Controller('auth')
+@ApiTags('Autorizacion')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
+  @ApiCreatedResponse({ description: 'User Loged' })
+  @ApiUnauthorizedResponse({ description: 'Credenciales Invalidas' })
+  @ApiBody({ type: LoginUserDto })
   @Post('/login')
   async login(@GetUser() user: UserEntity) {
     return this.authService.login(user);
--- src/user/dto/login-user.dto.ts
+++ src/user/dto/login-user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, MinLength, MaxLength } from 'class-validator';
 
 export class LoginUserDto {
@@ -5,9 +6,17 @@ export class LoginUserDto {
   @IsNotEmpty()
   @MinLength(4)
   @MaxLength(30)
-  name: string;
+  @ApiProperty({
+    description: 'Nombre de usuario',
+    example: 'usuario1',
+  })
+  username: string;
   @IsNotEmpty()
   @MinLength(6)
   @MaxLength(250)
+  @ApiProperty({
+    description: 'Contraseña de usuario',
+    example: 'password',
+  })
   password: string;
 }
