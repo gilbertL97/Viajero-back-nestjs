7cd0c1d650cbb68ec34d9ea55ed97963e28d42de
Gilbert Luis <gilbertluis97@gmail.com>
2023-08-11 04:45:50 -0400

    refatorizando para crear un cosntructor

--- src/traveler/dto/responseErrorOrWarning.dto.ts
+++ src/traveler/dto/responseErrorOrWarning.dto.ts
@@ -1,6 +1,10 @@
 import { FileErrorsTravelerDto } from './fileErrorsTravelers.dto';
 
 export class ResponseErrorOrWarningDto {
+  constructor(error: FileErrorsTravelerDto[], isError: boolean) {
+    this.errorAndWarning = error;
+    this.containErrors = isError;
+  }
   errorAndWarning: FileErrorsTravelerDto[];
   containErrors: boolean;
 }
