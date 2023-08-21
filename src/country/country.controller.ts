0b76e9713c6372cd2437e3c1b2956f3a4ca1de25
Gilbert Luis <gilbertluis97@gmail.com>
2023-08-21 02:22:41 -0400

    creando la doc de paises

--- src/country/country.controller.ts
+++ src/country/country.controller.ts
@@ -16,11 +16,20 @@ import { CountryService } from './country.service';
 import { CreateCountryDto } from './dto/create-country.dto';
 import { UpdateCountryDto } from './dto/update-country.dto';
 import { CountryEntity } from './entities/country.entity';
-
+import {
+  ApiBearerAuth,
+  ApiExcludeEndpoint,
+  ApiOkResponse,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
+@ApiBearerAuth('access-token')
 @Controller('country')
+@ApiTags('Paises')
 @UseGuards(JwtAuthGuard)
 export class CountryController {
   constructor(private readonly countryService: CountryService) {}
+  @ApiExcludeEndpoint()
   @UseGuards(RolesGuard)
   @Roles(UserRole.ADMIN)
   @Post()
@@ -30,6 +39,10 @@ export class CountryController {
     const data = await this.countryService.create(createCountryDto);
     return data;
   }
+  @ApiOkResponse({
+    description: 'Devuelve una lista de paises',
+    type: CreateCountryDto,
+  })
   @UseGuards(RolesGuard)
   @Roles(
     UserRole.ADMIN,
@@ -44,6 +57,16 @@ export class CountryController {
     const data = await this.countryService.findAll();
     return data;
   }
+  @ApiOkResponse({
+    type: CreateCountryDto,
+  })
+  @ApiParam({
+    description:
+      'recibe un ISO de 3 caracteres y devuelve todos los datos del pais',
+    type: 'string',
+    name: 'id',
+    example: 'CUB',
+  })
   @UseGuards(RolesGuard)
   @Roles(
     UserRole.ADMIN,
@@ -59,6 +82,7 @@ export class CountryController {
     const data = await this.countryService.findOne(iso);
     return data;
   }
+  @ApiExcludeEndpoint()
   @UseGuards(RolesGuard)
   @Roles(UserRole.ADMIN)
   @Patch(':id')
@@ -70,6 +94,7 @@ export class CountryController {
     const data = await this.countryService.update(iso, updateCountryDto);
     return data;
   }
+  @ApiExcludeEndpoint()
   @UseGuards(RolesGuard)
   @Roles(UserRole.ADMIN)
   @Delete(':id')
--- src/country/dto/create-country.dto.ts
+++ src/country/dto/create-country.dto.ts
@@ -1,18 +1,39 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateCountryDto {
+  @ApiProperty({
+    description: 'ISO de 2 caracteres',
+    example: 'CU',
+  })
   @IsString()
   @MaxLength(2)
   @MinLength(2)
   iso2: string;
+  @ApiProperty({
+    description: 'ISO de 3 caracteres',
+    example: 'CUB',
+  })
   @IsString()
   @MaxLength(3)
   @MinLength(3)
   iso: string;
+  @ApiProperty({
+    description: 'Nombre Largo',
+    example: 'La Republica de Cuba',
+  })
   @IsOptional()
   nombre_largo: string;
+  @ApiProperty({
+    description: 'Nombre Corto',
+    example: 'Republica de Cuba',
+  })
   @IsOptional()
   nombre_corto: string;
+  @ApiProperty({
+    description: 'Nombre Comun',
+    example: 'Cuba',
+  })
   @IsString()
   @MinLength(4)
   nombre_comun: string;
