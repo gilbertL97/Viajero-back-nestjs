60232da0c23183d71223a982272ea429ddbe30bf
Gilbert Luis <gilbertluis97@gmail.com>
2023-08-21 16:49:47 -0400

    creando un usaurio ssitema para saber cuando se importan y quien en los ficheros

--- src/script/default-user.ts
+++ src/script/default-user.ts
@@ -2,14 +2,31 @@ import { getRepository } from 'typeorm';
 import { ConfigService } from '@nestjs/config';
 import { Configuration } from 'src/config/config.const';
 import { UserEntity } from 'src/user/entity/user.entity';
+import { UserRole } from 'src/user/user.role';
 
 const setDefaultUser = async (config: ConfigService) => {
   const userRepository = getRepository<UserEntity>(UserEntity);
+  const systemUser = await userRepository
+    .createQueryBuilder()
+    .where('name = :name', {
+      name: 'system',
+    })
+    .getOne();
+  if (!systemUser) {
+    const sytem = userRepository.create({
+      name: 'system',
+      email: 'system' + config.get<string>(Configuration.DEFAULT_ADMIN_email),
+      password: config.get<string>(Configuration.DEFAULT_ADMIN_email),
+      role: UserRole.CLIENT,
+      active: true,
+    }); //el usuario q se usa para hacer las tareas automaticas
+    await userRepository.save(sytem);
+  }
 
   const defaultUser = await userRepository
     .createQueryBuilder()
-    .where('name = :email', {
-      email: config.get<string>(Configuration.DEFAULT_ADMIN_USER),
+    .where('name = :name', {
+      name: config.get<string>(Configuration.DEFAULT_ADMIN_USER),
     })
     .getOne();
 
@@ -18,7 +35,7 @@ const setDefaultUser = async (config: ConfigService) => {
       name: config.get<string>(Configuration.DEFAULT_ADMIN_USER),
       email: config.get<string>(Configuration.DEFAULT_ADMIN_email),
       password: config.get<string>(Configuration.DEFAULT_ADMIN_PASSW),
-      role: 'admin',
+      role: UserRole.ADMIN,
       active: true,
     });
 
