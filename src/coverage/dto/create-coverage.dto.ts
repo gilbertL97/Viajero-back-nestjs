d1aba2a12a0aefc0db8267bebbc3c4887486e723
Gilbert Luis <gilbertluis97@gmail.com>
2023-08-21 02:22:58 -0400

    comenzando lA doc de coberturas

--- src/coverage/dto/create-coverage.dto.ts
+++ src/coverage/dto/create-coverage.dto.ts
@@ -1,38 +1,63 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsString,
   IsNotEmpty,
   MinLength,
   MaxLength,
-  IsNumber,
-  IsBoolean,
   IsNumberString,
   IsOptional,
   IsBooleanString,
 } from 'class-validator';
 
 export class CreateCoverageDto {
+  @ApiProperty({
+    description: 'nombre',
+    example: 'Garantia de Viaje',
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(4)
   @MaxLength(50)
   name: string;
 
+  @ApiProperty({
+    description: 'Precio',
+    example: '25',
+  })
   @IsNumberString()
   @IsNotEmpty()
   price: number;
 
+  @ApiProperty({
+    description: 'SI es diaria la tarifa',
+    example: 'false',
+  })
   @IsBooleanString()
   @IsNotEmpty()
   daily: boolean;
 
+  @ApiProperty({
+    description: 'Precio alto riesgo',
+    example: '25',
+  })
   @IsNumberString()
   @IsNotEmpty()
   high_risk: number;
 
+  @ApiProperty({
+    description: 'Cantidad de Dias',
+    example: '30',
+    required: false,
+  })
   @IsOptional()
   @IsNumberString()
   number_of_days: number;
 
+  @ApiProperty({
+    description: 'Cadena de Configuracion',
+    example: 'garant+viaje',
+    required: false,
+  })
   @IsOptional()
   @IsString()
   @MinLength(4)
