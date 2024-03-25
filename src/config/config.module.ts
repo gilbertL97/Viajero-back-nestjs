6ded1261868229218082dfd40ca71caab58a3960
Gilbert Luis <gilbertluis97@gmail.com>
2024-03-25 01:11:41 -0400

    Se agrego las configuraciones directo de bd

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,15 +1,18 @@
-import { Module, OnModuleInit } from '@nestjs/common';
+import { Module, OnModuleInit, forwardRef } from '@nestjs/common';
 import { CustomConfigService } from './service/config.service';
 import { ConfigController } from './config.controller';
 import { UserModule } from 'src/user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigEntity } from './entities/config.entity';
 import { ConfigService } from '@nestjs/config';
-import defaultConfig from './service/InitConfig';
+//import defaultConfig from './service/InitConfig';
 @Module({
   controllers: [ConfigController],
   providers: [CustomConfigService],
-  imports: [UserModule, TypeOrmModule.forFeature([ConfigEntity])],
+  imports: [
+    forwardRef(() => UserModule),
+    TypeOrmModule.forFeature([ConfigEntity]),
+  ],
   exports: [CustomConfigService],
 })
 export class CustomConfigModule implements OnModuleInit {
