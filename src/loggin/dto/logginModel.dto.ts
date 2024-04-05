58906b99e80908f1aae370f41927835dfc8e1aae
Gilbert Luis <gilbertluis97@gmail.com>
2024-04-05 16:43:20 -0400

    cambiando el lugar para cargar datos de la request

--- src/loggin/dto/logginModel.dto.ts
+++ src/loggin/dto/logginModel.dto.ts
@@ -2,11 +2,6 @@ export class LogginModel {
   message: string;
   context: string;
   level: string;
-  userAgent: string;
-  requestId: string;
-  ip: string;
-  method: string;
-  url: string;
   userId?: number;
   createdAt: string;
   errorStack?: string;
