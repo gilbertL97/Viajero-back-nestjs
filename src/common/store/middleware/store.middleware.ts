4237fc61528d8b826f051e9f1c3dcbaceb97c9b4
Gilbert Luis <gilbertluis97@gmail.com>
2024-04-12 13:26:47 -0400

    decodifACNDO EL JWT EN EL store

--- src/common/store/middleware/store.middleware.ts
+++ src/common/store/middleware/store.middleware.ts
@@ -3,6 +3,7 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 import { AsyncLocalStorage } from 'async_hooks';
 import { Request, Response, NextFunction } from 'express';
 import { StoreModel } from '../model/Store.model';
+import { extract_user_data, jwtDecode } from 'src/common/utils/jwt/util.jwt';
 
 @Injectable()
 export class storeData implements NestMiddleware {
@@ -12,6 +13,8 @@ export class storeData implements NestMiddleware {
     const { requestId, ip, protocol, originalUrl, method } = req;
     const userAgent = req.get('user-agent');
     const url = `${protocol}://${req.get('Host')}${originalUrl}`;
+    const token = req.get('Authorization')?.replace('Bearer ', '');
+    if (token) console.log(jwtDecode(token));
     const store: StoreModel = {
       userAgent,
       requestId,
