52ecdd867bbbd3be8fcbf0e3f650c8a7c8703397
Gilbert Luis <gilbertluis97@gmail.com>
2024-04-15 12:48:06 -0400

    guardando logs de usuarios

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -16,6 +16,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { UserEntity } from './entity/user.entity';
 import { UserRole } from './user.role';
 import { Configuration } from 'src/config/config.const';
+import { LogginService } from 'src/loggin/loggin.service';
 
 @Injectable()
 export class UserService {
@@ -24,13 +25,26 @@ export class UserService {
     private readonly userRepository: Repository<UserEntity>,
     @Inject(forwardRef(() => ContractorService))
     private readonly contractorService: ContractorService,
+    private readonly loggingService: LogginService,
   ) {}
 
   async getUsers(): Promise<UserEntity[]> {
+    await this.loggingService.create({
+      message: 'Obteniendo todos los usuarios',
+      context: 'UserService',
+      level: 'info',
+      createdAt: new Date().toISOString(),
+    });
     return await this.userRepository.find({ relations: ['contractors'] });
   }
 
   async getUser(id: number): Promise<UserEntity> {
+    await this.loggingService.create({
+      message: `Obteniendo usuario con id: ${id}`,
+      context: 'UserService',
+      level: 'info',
+      createdAt: new Date().toISOString(),
+    });
     const user: UserEntity = await this.userRepository.findOne({
       where: { id: id },
       relations: ['contractors'],
@@ -40,6 +54,12 @@ export class UserService {
     return user;
   }
   async getUserWithPass(id: number): Promise<UserEntity> {
+    await this.loggingService.create({
+      message: `Obteniendo usuario con id: ${id} con datos completos`,
+      context: 'UserService',
+      level: 'info',
+      createdAt: new Date().toISOString(),
+    });
     const user: UserEntity = await this.userRepository.findOne({
       where: { id: id },
       select: ['id', 'name', 'password', 'email', 'role'],
@@ -62,6 +82,12 @@ export class UserService {
         user.contractors = [contrator];
       }
     } else delete userDto.contractor;
+    await this.loggingService.create({
+      message: 'Dando de alta a un usuario',
+      context: 'UserService',
+      level: 'info',
+      createdAt: new Date().toISOString(),
+    });
     const newUser = await this.userRepository.save(user).catch(() => {
       throw new BadRequestException('duplicate name or email');
     });
@@ -87,6 +113,12 @@ export class UserService {
         delete editedUser.contractor;
       }
     } else delete updateUserDto.contractor;
+    await this.loggingService.create({
+      message: 'Actualizando usuario',
+      context: 'UserService',
+      level: 'info',
+      createdAt: new Date().toISOString(),
+    });
     return await this.userRepository.save(editedUser);
   }
   async updateProfile(
@@ -99,6 +131,12 @@ export class UserService {
     if (updateProfile.passwordNew1 !== updateProfile.passwordNew2)
       throw new UnauthorizedException('The passwords do not match');
     user.password = updateProfile.passwordNew1;
+    await this.loggingService.create({
+      message: 'Actualizando perfil',
+      context: 'UserService',
+      level: 'info',
+      createdAt: new Date().toISOString(),
+    });
     const profiel = await this.userRepository.save(user);
     delete profiel.password;
     return profiel;
@@ -109,6 +147,12 @@ export class UserService {
     if (user) return await this.userRepository.remove(user);
   }
   async findUserByName(name: string): Promise<UserEntity> {
+    await this.loggingService.create({
+      message: 'Eliminando usuario',
+      context: 'UserService',
+      level: 'info',
+      createdAt: new Date().toISOString(),
+    });
     return await this.userRepository
       .createQueryBuilder('usuarios')
       .where({ name })
@@ -132,9 +176,21 @@ export class UserService {
   async updateRefreshToken(id: number, token: string) {
     const user = await this.getUser(id);
     user.refresh_token = token;
+    await this.loggingService.create({
+      message: 'Actualizando el refresh token',
+      context: 'UserService',
+      level: 'info',
+      createdAt: new Date().toISOString(),
+    });
     return (await this.userRepository.save(user)).refresh_token;
   }
   async getToken(id: number) {
+    await this.loggingService.create({
+      message: 'Obteniendo el token desde el usuario',
+      context: 'UserService',
+      level: 'info',
+      createdAt: new Date().toISOString(),
+    });
     return await this.userRepository.findOne({
       where: { id: id },
       select: ['refresh_token', 'name', 'role', 'id'],
