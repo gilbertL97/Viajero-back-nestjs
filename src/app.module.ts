9b862a34790e53f91b84523c6134e2a99e7a2476
Gilbert Luis <gilbertluis97@gmail.com>
2024-04-15 12:49:39 -0400

    registrando el filtro global

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,13 +16,14 @@ import { FileModule } from './file/file.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
-import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { CustomConfigModule } from './config/config.module';
 import { storeData } from './common/store/middleware/store.middleware';
 import { LogginModule } from './loggin/loggin.module';
 import { RequestLogginMiddleware } from './loggin/middleware/requestLogginMiddleware';
 
 import { LogginResponseInterceptor } from './loggin/interceptor/responseLoggin.interceptor';
+import { HttpExceptionFilterLog } from './loggin/filter/ExceptionFilterLog';
 
 @Module({
   imports: [
@@ -57,6 +58,10 @@ import { LogginResponseInterceptor } from './loggin/interceptor/responseLoggin.i
     {
       provide: APP_INTERCEPTOR,
       useClass: LogginResponseInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilterLog,
     },
   ],
 })
