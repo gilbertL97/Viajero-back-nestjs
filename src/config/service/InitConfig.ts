682ce7dcd65de433ddda173f4b568d4391ffb5cd
Gilbert Luis <gilbertluis97@gmail.com>
2024-06-06 18:21:10 -0400

    agrand log de ususario sistmea

--- src/config/service/InitConfig.ts
+++ src/config/service/InitConfig.ts
@@ -10,21 +10,38 @@ async function userDefault(
   configUser: UserService,
   logginService: LogginService,
 ): Promise<void> {
-  logginService.create({
-    message: `Insertando usuario por defecto`,
-    context: 'InitConfig',
-    level: 'info',
-    createdAt: new Date().toISOString(),
-  });
-  const systemUser = await configUser.findUserByName('system');
+  let systemUser = await configUser.findUserByName('system');
+
   if (!systemUser)
-    await configUser.createUser({
+    systemUser = await configUser.createUser({
       name: 'system',
       email: 'system' + config.get<string>(Configuration.DEFAULT_ADMIN_email),
       password: config.get<string>(Configuration.DEFAULT_ADMIN_PASSW),
       role: UserRole.ADMIN,
       contractor: undefined,
     });
+
+  await logginService.saveLog({
+    message: 'Inicializando usuario sistema',
+    context: 'InitConfig',
+    level: 'info',
+    createdAt: new Date().toISOString(),
+    errorStack: '-',
+    userAgent: '-',
+    requestId: '-',
+    ip: '-',
+    method: '',
+    url: '-',
+    userId: systemUser.id,
+  });
+
+  await logginService.create({
+    message: `Creando o buscando  usuario del sistema`,
+    context: 'InitConfig',
+    level: 'info',
+    createdAt: new Date().toISOString(),
+  });
+
   const defaultUser = await configUser.findUserByName(
     config.get<string>(Configuration.DEFAULT_ADMIN_USER),
   );
@@ -36,54 +53,24 @@ async function userDefault(
       role: UserRole.ADMIN,
       contractor: undefined,
     });
-  // const userRepository = getRepository<UserEntity>(UserEntity);
-  // const systemUser = await userRepository
-  //   .createQueryBuilder()
-  //   .where('name = :name', {
-  //     name: 'system',
-  //   })
-  //   .getOne();
-  // if (!systemUser) {
-  //   const sytem = userRepository.create({
-  //     name: 'system',
-  //     email: 'system' + config.get<string>(Configuration.DEFAULT_ADMIN_email),
-  //     password: config.get<string>(Configuration.DEFAULT_ADMIN_PASSW),
-  //     role: UserRole.ADMIN,
-  //     active: true,
-  //   }); //el usuario q se usa para hacer las tareas automaticas
-  //   await userRepository.save(sytem);
-  // }
-
-  // const defaultUser = await userRepository
-  //   .createQueryBuilder()
-  //   .where('name = :name', {
-  //     name: config.get<string>(Configuration.DEFAULT_ADMIN_USER),
-  //   })
-  //   .getOne();
-
-  // if (!defaultUser) {
-  //   const adminUser = userRepository.create({
-  //     name: config.get<string>(Configuration.DEFAULT_ADMIN_USER),
-  //     email: config.get<string>(Configuration.DEFAULT_ADMIN_email),
-  //     password: config.get<string>(Configuration.DEFAULT_ADMIN_PASSW),
-  //     role: UserRole.ADMIN,
-  //     active: true,
-  //   });
-
-  //   return await userRepository.save(adminUser);
-  // }
+  await logginService.saveLog({
+    message: 'Inicializando usuario Aministrador por defecto',
+    context: 'InitConfig',
+    level: 'info',
+    createdAt: new Date().toISOString(),
+    errorStack: '-',
+    userAgent: '-',
+    requestId: '-',
+    ip: '-',
+    method: '',
+    url: '-',
+    userId: defaultUser.id,
+  });
 }
 async function setDefaultFilePath(
   config: ConfigService,
   customConfigService: CustomConfigService,
-  logginService: LogginService,
 ) {
-  logginService.create({
-    message: `Inertando configuracion por defecto de las carpetas`,
-    context: 'InitConfig',
-    level: 'info',
-    createdAt: new Date().toISOString(),
-  });
   await Promise.all([
     findOrCreateConfig(Configuration.FILES_PATH, config, customConfigService),
     findOrCreateConfig(Configuration.TEMP_FILE, config, customConfigService),
@@ -125,7 +112,7 @@ async function defaultConfig(
 ) {
   await Promise.all([
     userDefault(config, configUser, logginService),
-    setDefaultFilePath(config, customConfigService, logginService),
+    setDefaultFilePath(config, customConfigService),
   ]);
 }
 export default defaultConfig;
